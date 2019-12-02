2f03c028e2eee35c15ff151497572c3fb69a6f52
Miguel Mendez <keloxers@gmail.com>
2019-12-02 11:07:28 -0300

    go

--- config/app.php
+++ config/app.php
@@ -28,6 +28,12 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),        
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
